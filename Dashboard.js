cdd31333d9da7170625ff256015c44fe0a1a312d
Sachin Mule <127710465+SachinMule7890@users.noreply.github.com>
2024-10-13 17:09:09 +0530

    components

---
+++ Dashboard.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Dashboard() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h2>Welcome to the Dashboard!</h2>
+        <p>You are logged in.</p>
+      </header>
+    </div>
+  );
+}
+
+export default Dashboard;
